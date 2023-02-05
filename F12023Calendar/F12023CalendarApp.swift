41d9a3e2f1c56a9b152d20d5bbc965a0acb10428
Andrey <studenkoff@gmail.com>
2023-02-05 16:59:08 +0200

    Initial Commit

---
+++ F12023Calendar/F12023CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  F12023CalendarApp.swift
+//  F12023Calendar
+//
+//  Created by Andrey Studenkov on 05.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct F12023CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
